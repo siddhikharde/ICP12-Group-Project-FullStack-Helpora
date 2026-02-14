5b9b921210f36d9afe07dbda753b1d8f56ebef7f
siddhikharde <siddhikharde2005@gmail.com>
2026-02-14 13:25:33 +0530

    header created

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>
-      Register
+    <div className='min-h-screen '>
+      <div className='flex justify-center flex-col items-center mx-auto my-10 w-[90%] md:w-[500px] p-5 bg-[rgb(250 205 71)] shadow-lg rounded-lg gap-3'>
+        <h1 className='text-3xl font-bold text-[#2b92f3]'>Welcome to Helpora</h1>
+        <p className='text-lg text-[#554d47]'>Find and book trusted local service providers</p>
+      </div>
     </div>
   )
 }
