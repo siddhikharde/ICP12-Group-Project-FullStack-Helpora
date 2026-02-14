012b9875c699950c173880fffe0c4e26a55ef8b2
siddhikharde <siddhikharde2005@gmail.com>
2026-02-14 11:37:19 +0530

    User Schema created

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
 app.get('/health',(req,res)=>{
   res.send('Server is healthy');
 })
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+ const userSchema = new Schema({
+   username:{
+    type:String,
+    required:true,
+
+   },
+   email:{
+    type:String,
+    required:true,
+    unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phoneNo:{
+        type:String,
+        required:true
+ },service:{
+    default:"Book a service",
+    type:String,
+    required:true
+ }
+ })
+
+const User = model("User",userSchema);
+
+export default User;
