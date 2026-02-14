f63d9983394d90e7329bc5c7ebbadd8208e55058
siddhikharde <siddhikharde2005@gmail.com>
2026-02-14 11:27:00 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'; 
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const connectDB =async ()=>{
+   const con=await mongoose.connect(process.env.MONGODB_URI)
+   console.log("Connected to MongoDB");
+}
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the HELPORA...');
+});
+
+app.get('/health',(req,res)=>{
+  res.send('Server is healthy');
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
