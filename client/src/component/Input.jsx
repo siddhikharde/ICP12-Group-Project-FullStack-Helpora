bda04bcae42c75b075801cbb76b388e2ae8293ef
siddhikharde <siddhikharde2005@gmail.com>
2026-02-16 23:49:03 +0530

    form created for update the profile info

--- client/src/component/Input.jsx
+++ client/src/component/Input.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Input({placeholder, type="text", value, onChange}) {
+function Input({placeholder, type="text", value, disabled = false, onChange}) {
   return (
     <div>
      <input 
       type={type}
       placeholder={placeholder}
       value={value}
+      disabled={disabled}
       onChange={onChange}
       className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -2,58 +2,101 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../component/Navbar'
 import UserImg from '../assets/user.png'
 import Button from '../component/Button';
+import Input from '../component/Input';
 
 function Profile() {
-    const [isEditing, setIsEditing]=useState(false)
-      const user=JSON.parse(localStorage.getItem("user"));
-      const [userData, setUserData]=useState({
-        name:user.fullName,
-        email:user.email,
-        phoneNo:user.phoneNo
+    const [isEditing, setIsEditing] = useState(false)
+    const user = JSON.parse(localStorage.getItem("user"));
+    const [userData, setUserData] = useState({
+        name: user.fullName,
+        email: user.email,
+        phone: user.phoneNo
 
-      })
- useEffect(()=>{
- console.log(userData)
- },[])
-  return (
-    <div className='min-h-screen bg-gray-50 text-[#2a2e32]'>
-        <Navbar/>
-        <div className=' max-w-full mx-auto mt-16 px-6'>
-            <div className='bg-white rounded-2xl shadow-xl p-10 mt-20'>
-               <div className='flex flex-col md:flex-row items-center gap-8'>
-                 <div className="relative">
-              <img
-                src={`${UserImg}`}
-                alt="Profile"
-                className="w-36 h-36 rounded-full object-cover border-4 border-[#2b92f3]"
-              />
-            </div>
+    })
+    useEffect(() => {
+        console.log(userData)
+    }, [])
+    return (
+        <div className='min-h-screen bg-gray-50 text-[#2a2e32]'>
+            <Navbar />
+            <div className=' max-w-full mx-auto mt-16 px-6'>
+                <div className='bg-white rounded-2xl shadow-xl p-10 mt-20'>
+                    <div className='flex flex-col md:flex-row items-center gap-8'>
+                        <div className="relative">
+                            <img
+                                src={`${UserImg}`}
+                                alt="Profile"
+                                className="w-36 h-36 rounded-full object-cover border-4 border-[#2b92f3]"
+                            />
+                        </div>
 
-            <div className='flex-col text-center md:text-left'>
-                <h2 className="text-3xl font-bold text-[#2b92f3]"> 
-                    {userData.name}
-                </h2>
-                <p className="text-[#554d47] mt-2">
-                Manage your personal information
-              </p>
-            </div>
-            <div>
-             {
-                !isEditing && (
-                    <Button title="Edit Profile" variant="primary"
-                    onClick={()=>{
-                        setIsEditing(true)
-                    }}/>
-                )
-               }
-            </div>
-               </div>
+                        <div className='flex-col text-center md:text-left'>
+                            <h2 className="text-3xl font-bold text-[#2b92f3]">
+                                {userData.name}
+                            </h2>
+                            <p className="text-[#554d47] mt-2">
+                                Manage your personal information
+                            </p>
+                        </div>
+                        <div>
+                            {
+                                !isEditing && (
+                                    <Button title="Edit Profile" variant="primary"
+                                        onClick={() => {
+                                            setIsEditing(true)
+                                        }} />
+                                )
+                            }
+                        </div>
+                    </div>
+                    
+                     <div className="border-t my-8"></div>
+                     <div className="grid md:grid-cols-2 gap-6">
+                        <div className="flex flex-col gap-2">
+                             <label className="font-semibold">Full Name</label>
+                             <Input value={userData.name}
+                             onChange={(e)=>{
+                                setUserData({
+                                    ...userData, name:e.target.value
+                                })
+                             }}
+                disabled={!isEditing}/>
+                        </div>
+
+                         <div className="flex flex-col gap-2">
+                             <label className="font-semibold">Email</label>
+                             <Input value={userData.email}  onChange={(e)=>{
+                                setUserData({
+                                    ...userData, email:e.target.value
+                                })
+                             }}
+                disabled={!isEditing}/>
+                        </div>
+
+                         <div className="flex flex-col gap-2">
+                             <label className="font-semibold">Phone Number</label>
+                             <Input value={userData.phone}  onChange={(e)=>{
+                                setUserData({
+                                    ...userData, phone:e.target.value
+                                })
+                             }}
+                disabled={!isEditing}/>
+                        </div>
+
+                     </div>
+                     {isEditing && (
+                        <div className='mt-8 text-righ'>
+                            <Button title="Save Changes" onClick={()=>{
+                                setIsEditing(false)
+                            }}/>
+                            </div>
+                     )}
+                </div>
+
             </div>
 
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Profile
