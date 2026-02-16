81d828501309baa55bd80f3d77c92b94c2ca3904
siddhikharde <siddhikharde2005@gmail.com>
2026-02-16 18:44:31 +0530

    contact form created

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import Navbar from "../component/Navbar";
 import {Mail, MapPinHouse, Phone } from "lucide-react";
+import Input from "../component/Input";
+import Button from "../component/Button";
 function Contact() {
-  
+const [user, setUser]=useState({
+  email:"",
+  password:""
+})
 
   return (
     <div className="min-h-screen bg-gray-50 text-textMain px-6 py-16">
@@ -16,13 +21,13 @@ function Contact() {
         </p>
       </div>
 
-      <div className="max-w-6xl mx-auto flex  gap-10">
-        <div className="bg-white p-8 rounded-2xl w-1/2 shadow-lg">
+      <div className="max-w-full  md:px-10 mx-auto flex  flex-col md:flex-row  gap-10">
+        <div className="bg-white p-8 h-auto rounded-2xl md:w-1/2 w-full shadow-lg">
           <h2 className="text-2xl font-semibold text-[#2b92f3] mb-6">
             Get In Touch
           </h2>
 
-          <div className="space-y-5 text-textMain">
+          <div className="space-y-5 ">
             <p className=" flex items-center gap-2 cursor-pointer">
               <span className="font-semibold flex items-center gap-1 "><MapPinHouse size={17} strokeWidth={2.5} className="text-red-800" /> Address:</span> Loni Pravara, India
             </p>
@@ -36,8 +41,46 @@ function Contact() {
             </p>
           </div>
         </div>
+
+
+        <div className="bg-white p-8 rounded-2xl md:w-1/2 w-full shadow-lg">
+        <h3 className="text-2xl font-semibold mb-6  text-[#2b92f3] ">Send a Message</h3>
+       <div className='w-full p-3 flex flex-col gap-5  '>
+         <div className='flex flex-col gap-2'>
+            <label className=' text-[15px] text-gray-700 ' >Name</label>
+            <Input placeholder='Your Name' type='text'  onChange={(e)=>{
+              
+            }} />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label className=' text-[15px] text-gray-700 ' >Email</label>
+            <Input placeholder='you@example.com' type='email'  onChange={(e)=>{
+              
+            }} />
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <label className=' text-[15px] text-gray-700 ' >Subject</label>
+            <Input placeholder='How can we help you ?' type='email'  onChange={(e)=>{
+              
+            }} />
+          </div>
+           <div className='flex flex-col gap-2 mb-3'>
+            <label className=' text-[15px] text-gray-700 ' >Message</label>
+            <textarea
+            placeholder="Your Message" rows="4" required      className="w-full border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+          </div>
+          <Button title={" Send Message"} />
        </div>
-    </div>
+          
+       </div>
+       </div>
+
+       
+
+      </div>
+    
+ 
   );
 }
 
