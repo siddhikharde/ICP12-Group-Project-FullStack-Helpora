667cc135830a912439eede0426a197e6a75e7c5c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-18 16:54:30 +0530

    Schema designed

--- server/models/servicemen.js
+++ server/models/servicemen.js
@@ -1,1 +1,31 @@
 import {Schema, model} from 'mongoose';
+
+const servicemenSchema = new Schema({
+    field:{
+        type:String,
+        required:true
+    },
+    experience:{
+        type:Number,
+        required:true
+    },
+    skills:{
+        type:[String],
+        required:true
+    },
+    availability:{
+        type:Boolean,
+        default:true
+    },
+    certifications:{
+        type:[String],
+    },
+    professionalSummary:{
+        type:String,
+    },
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    }
+})
