ef243f6dac04986a115c7f1a9298bacfe3acc6de
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-18 17:13:00 +0530

    getServicemenProfile controller created

--- server/controllers/servicemen.js
+++ server/controllers/servicemen.js
@@ -4,3 +4,25 @@ import jwt from "../middleware/jwt";
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const getServicemenProfile=async (req,res)=>{
+    try{
+        const {fullname, email,phoneNo}=req.user;
+        return res.json({
+            success:true,
+            message:"Servicemen profile retrieved successfully",
+            data:{
+                fullname,
+                email,
+                phoneNo
+            }
+        })
+    }
+    catch(e){
+        return res.json({
+            success:false,
+            message:"Failed to retrieve servicemen profile",
+            error:{message:e.message}
+        })
+    }
+}
