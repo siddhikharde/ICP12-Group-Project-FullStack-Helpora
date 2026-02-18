fd16730d01a60f384a8a06c15a6c7948173b0e54
AtharvBolke99 <atharvbolke99@gmail.com>
2026-02-19 01:08:45 +0530

    service page design done also service info page done

---
+++ client/src/component/Servicecard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { MapPin, Clock } from "lucide-react";
+import Button from "../component/Button";
+
+function Servicecard({img, name, profession, location, experience, price}) {
+  return (
+    <div className="border w-70 relative border-gray-300 rounded-xl cursor-pointer shadow-lg">
+      <img
+        src={img}
+        width={290}
+        className="rounded-t-xl"
+      />
+      <div className="p-4">
+        <p className="text-xl font-bold">{name}</p>
+        <p className="text-gray-500">{profession}</p>
+        <p className="absolute top-3 right-4 bg-green-400 cursor-pointer px-3 text-white py-1 rounded-2xl">
+          Verified
+        </p>
+        <div className="flex items-center my-2">
+          <MapPin size={20} className="mr-2 text-gray-500" />
+          <p className="text-gray-500">{location}</p>
+        </div>
+        <div className="flex items-center my-2">
+          <Clock size={20} className="mr-2 text-gray-500" />
+          <p className="text-gray-500">{experience} years of experience.</p>
+        </div>
+        <div className="flex justify-between items-center mt-5">
+          <p className="text-[#2b92f3] font-bold text-2xl">₹ {price}/hr</p>
+          <Button
+            title={"View Profile"}
+            size="sm"
+            onClick={() => {
+              window.location.href = "/serviceinfo";
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Servicecard;
