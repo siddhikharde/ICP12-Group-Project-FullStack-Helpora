e17bf50c44c117453b99f6c96043b77ff4e9d465
siddhikharde <siddhikharde2005@gmail.com>
2026-02-22 19:59:03 +0530

    routes changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import  Home  from './views/Home.jsx'
+import  Login  from './views/Login.jsx'
+import Register from './views/Register.jsx'
+import Service from './views/Service.jsx'
+import Contact from './views/Contact.jsx'
+import About from './views/About.jsx'
+import Profile from './views/Profile.jsx'
+import ServicemenProfile from './views/ServicemenProfile.jsx'
+import Serviceinfo from './views/Serviceinfo.jsx'
 
 function App() {
   return (
-    <div>
-      APP
-    </div>
+   <BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/register' element={<Register/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/service' element={<Service/>}/>
+  <Route path='/serviceinfo/:id' element={<Serviceinfo/>}/>
+  <Route path='/contact' element={<Contact/>}/>
+  <Route path='/profile' element={<Profile/>}/>
+  <Route path='/servicemen-profile' element={<ServicemenProfile/>}/>
+</Routes>
+</BrowserRouter>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,36 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import  Home  from './views/Home.jsx'
-import  Login  from './views/Login.jsx'
-import Register from './views/Register.jsx'
-import User from './views/user/User.jsx'
-import Provider from './views/serviceProvider/Provider.jsx'
-import Service from './views/Service.jsx'
-import Contact from './views/Contact.jsx'
-import About from './views/About.jsx'
-import Profile from './views/Profile.jsx'
-import ServicemenProfile from './views/ServicemenProfile.jsx'
-import Serviceinfo from './views/Serviceinfo.jsx'
 
 
 
 const root=createRoot(document.getElementById('root'))
 root.render(
-<BrowserRouter>
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/login' element={<Login/>}/>
-  <Route path='/register' element={<Register/>}/>
-  <Route path='/user' element={<User/>}/>
-  <Route path='/service-provider' element={<Provider/>}/>
-  <Route path='/about' element={<About/>}/>
-  <Route path='/service' element={<Service/>}/>
-  <Route path='/serviceinfo/:id' element={<Serviceinfo/>}/>
-  <Route path='/contact' element={<Contact/>}/>
-  <Route path='/profile' element={<Profile/>}/>
-  <Route path='/servicemen-profile' element={<ServicemenProfile/>}/>
-</Routes>
-</BrowserRouter>
+<App/>
 )
