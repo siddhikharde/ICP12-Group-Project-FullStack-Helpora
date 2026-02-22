cd96f542719a514ec0d0f35c8d0667e0761062e5
siddhikharde <siddhikharde2005@gmail.com>
2026-02-22 18:00:55 +0530

    serch filter added

--- client/src/views/Service.jsx
+++ client/src/views/Service.jsx
@@ -2,10 +2,12 @@ import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
 import Servicecard from "../component/Servicecard";
 import { useEffect, useState } from "react";
+import Input from "../component/Input"
 import axios from "axios";
 
 function Service() {
   const [serviceData, setServiceData] = useState([]);
+  const [searchItem, setSearchItem]=useState("");
   const loadData = async () => {
     const responce = await axios.get(
       `${import.meta.env.VITE_API_BASE_URL}/servicemens`,
@@ -14,6 +16,13 @@ function Service() {
     setServiceData(responce.data.data);
   };
 
+  const filteredServices=serviceData.filter((service)=>{
+    const item=searchItem.toLowerCase();
+    return(
+      service.field.toLowerCase().includes(item) ||
+      service.serviceAreas?.join(" ").toLowerCase().includes(item)
+    )
+  })
   useEffect(() => {
     loadData();
   }, []);
@@ -29,8 +38,18 @@ function Service() {
           Find and book the right professional for your needs
         </p>
       </div>
+      <div className="px-5 py-10">
+        <Input
+        placeholder={"Search by location or srvice..."}
+        type="text"
+        value={searchItem}
+        onChange={(e)=>{
+          setSearchItem(e.target.value)
+        }}
+        />
+      </div>
       <div className="flex justify-center items-center gap-10 flex-wrap mb-20">
-        {serviceData.map((service, idx) => {
+        {filteredServices.map((service, idx) => {
           return (
             <Servicecard
               key={service._id}
